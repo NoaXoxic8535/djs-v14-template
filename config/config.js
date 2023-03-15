d0705d18cecbc46a5cda82283aaa21ee6e578aa2
NoaXoxic8535 <127994430+NoaXoxic8535@users.noreply.github.com>
2023-03-15 21:17:43 +0000

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  Prefix: "?", // Bot prefix
+  Users: {
+    OWNERS: ["1085658584064467026", "1085658584064467026"] // The owners id
+  },
+  Handlers},: {
+    MONGO: "" // Your mongodb
+  },
+  Client: {
+    TOKEN: "", // Your bot token
+    ID: "" // Your Bot ID
+  }
+
+}
+
+/************************************
+ * Bot Owner: @NoaXoxic8535
+ * Template Made By: discord.gg/gkvwcrDS5Y
+ * Everything is Setup for you Just add commands and it will work!
+ ************************************/
